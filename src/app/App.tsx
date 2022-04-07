4ab9e7fa02c94cb2c02810f532702f9efcac5d1e
Jonathan Monroy <jonathan.monroy@live.com>
2022-04-07 16:04:00 -0400

    Initial project structure

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <h1><img src="favicon.png" alt="logo dog" id="logo" />DOGS Ceo Challange</h1>
+    </div>
+  );
+}
+
+export default App;
